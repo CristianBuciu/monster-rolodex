3bd458438d29e59e8254c9b46fe7b75b70703f3a
Cristian Buciu <cristianbuciu@Cristians-iMac.local>
2019-09-27 17:44:04 +0200

    adding files for git pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
-import {SearchBox} from './components/search-box/search-box.component';
+import { SearchBox } from './components/search-box/search-box.component';
 import './App.css';
 
 class App extends Component {
 	constructor () {
 		super();
-		this.state = { monsters: [], searchField:'' };
+		this.state = { monsters: [], searchField: '' };
 	}
 
 	componentDidMount () {
@@ -14,12 +14,20 @@ class App extends Component {
 			.then((response) => response.json())
 			.then((users) => this.setState({ monsters: users }));
 	}
+
+	handleChange = (e) => {
+		this.setState({ searchField: e.target.value });
+	};
+
 	render () {
-    const {monsters, searchField} = this.state;
-    const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
+		const { monsters, searchField } = this.state;
+		const filteredMonsters = monsters.filter((monster) =>
+			monster.name.toLowerCase().includes(searchField.toLowerCase())
+		);
 		return (
 			<div className="App">
-				<SearchBox  placeholder='search monster' handleChange={(e) => this.setState({ searchField: e.target.value })} />
+      <h1 className='title'>Monster Rolodex</h1>
+				<SearchBox placeholder="search monster" handleChange={this.handleChange} />
 				<CardList monsters={filteredMonsters} />
 			</div>
 		);
